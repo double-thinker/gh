5cc5164582bc0519578e3481171935c3959d36e4
Alejandro Vidal <a@a.com>
2022-10-04 20:34:28 +0000

    sphinx format

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -7,20 +7,11 @@ def get_numerical_features(df):
     """get_numerical features devuelve una lista
     con el nombre de las columnas que contienen datos
     de tipo numérico
-    
-    Parámetros
-    ----------
 
-    df: dataframe
 
-    Ejemplos
-    --------
-
-    >>> from modeltools.preprocessing import get_numerical_features
-    >>> import pandas as pd
-    >>> df = pd.DataFrame({"a":[1]})
-    >>> get_numerical_features(df)
-    ['a']
-
+    :param df: dataframe
+    :type df: pandas.DataFrame
+    :return: lista de nombres de columnas
+    :rtype: List[str]
     """
     return list(df.select_dtypes(include=[np.number]).columns)
